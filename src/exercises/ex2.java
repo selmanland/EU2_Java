34de5e3cc07c958fd7b57933267d11bcdf7193d7
hiltas73 <h_iltas@yahoo.com>
2021-04-08 23:08:12 +0300

    after GR7 meet

--- src/exercises/ex2.java
+++ src/exercises/ex2.java
@@ -4,23 +4,15 @@ public class ex2 {
 
 	public static void main(String[] args) {
 		
-/*
-Write a program that outputs the number of hours, minutes, and seconds that 
-corresponds to input total seconds.
+		int cupMiligram=310;
+	    int NumberOfCup=0;
 
-	-Declare int variables inputSeconds, hours, minutes, seconds
-	-Initialize the inputSeconds 
-*/
-	
-		int inputSeconds, hours, minutes, seconds;
-		inputSeconds = 15895;
-		
-		hours = inputSeconds / 60 / 60;
-		minutes = (inputSeconds / 60) % 60;
-		seconds = inputSeconds % 60;
-		
-		System.out.println(inputSeconds + " seconds => " + hours + " hours " + minutes + " minutes and " + seconds + " seconds.");
-	
+
+
+	            for(NumberOfCup=0; NumberOfCup*cupMiligram<10000; NumberOfCup++) {
+
+	            }
+	            System.out.println("It would take about " + NumberOfCup + " drinks for a lethal overdose");
 	}
 	
 
