1b3f07d4f0c9f97e4310a51f77105f03f6daf7c4
hiltas73 <h_iltas@yahoo.com>
2021-04-15 15:10:10 +0300

    after q3

--- src/assignment8_Methods/q6_next3Numbers.java
+++ src/assignment8_Methods/q6_next3Numbers.java
@@ -7,14 +7,14 @@ public class q6_next3Numbers {
 	public static void main(String[] args) {
 		// q6 print next 3 numbers method
 		
-		next3(0);
+		next3();
 
 	}
 
-	public static int next3(int num) {
+	public static int next3() {
 		Scanner user_input = new Scanner(System.in);
 		System.out.println("enter number: ");
-		num = user_input.nextInt();
+		int num = user_input.nextInt();
 		System.out.println("next 3 are: ");
 		System.out.print((num+1) + " " + (num+2) + " " + (num+3));
 		
