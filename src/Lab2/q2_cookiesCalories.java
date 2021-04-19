504fe86e5e6e6678555bc72cf8fc0bb718620d1b
hiltas73 <h_iltas@yahoo.com>
2021-04-20 00:31:53 +0300

    after scanner practice

--- src/Lab2/q2_cookiesCalories.java
+++ src/Lab2/q2_cookiesCalories.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 public class q2_cookiesCalories {
 
 	public static void main(String[] args) {
+/*
+Q ???
+ */
 		// q2 Cookies Calories
 		// 300 calories for 40 cookies
 		// 7.5 calories for each
 
-		double perEach = 7.5;
+		double perEach = 4;
 
 		Scanner user_input = new Scanner(System.in);
 		System.out.println("Enter the number of cookies you ate : ");
--- src/exercises/ex2.java
+++ src/exercises/ex2.java
@@ -6,12 +6,21 @@ public class ex2 {
 
 	public static void main(String[] args) {
 		
-		Scanner sc=new Scanner(System.in);
-        System.out.println("provide a sentence and I will repeat it :");
-        //String str=sc.next();//if your sentence is a word without space
-        String str2=sc.nextLine();// if your sentence has spaces between
-        System.out.println(str2);
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of Females: ");
+        int female = sc.nextInt();
+
+        System.out.print("Enter number of Males: ");
+        int male = sc.nextInt();
+
+        double percentageOfFemale = ((double) female / (female + male)) * 100;
+        double percentageOfMale = ((double) male / (female + male)) * 100;
+
+        System.out.println("Percentage of Female is: " + percentageOfFemale);
+        System.out.println("Percentage of Male is: " + percentageOfMale);
 		
+        
+        sc.close();
 	}
 	
 
