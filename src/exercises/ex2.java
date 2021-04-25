00ffe93dbcb2e33f9d0230be95a452493c51d2f1
hiltas73 <h_iltas@yahoo.com>
2021-04-25 23:30:53 +0300

    after ex2

--- src/exercises/ex2.java
+++ src/exercises/ex2.java
@@ -3,37 +3,45 @@ package exercises;
 import java.util.Scanner;
 
 public class ex2 {
-
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter Item1: ");
-        String item1 = sc.nextLine();   
-        System.out.println("Enter Item1 price: ");
-        double price1 = sc.nextDouble();
-        sc.nextLine(); // above int below "string line", before "string line" must add this line
-
-        System.out.print("Enter Item2: ");
-        String item2 = sc.nextLine();
-        
-        System.out.println("Enter Item2 price: ");
-        double price2 = sc.nextDouble();
-        sc.nextLine(); // above int below "string line", before "string line" must add this line
-        
-        System.out.print("Enter Item3: ");
-        String item3 = sc.nextLine();
-        System.out.println("Enter Item3 price: ");
-        double price3 = sc.nextDouble();
-        
-        double totalPrice = price1 + price2 + price3;
-
-        System.out.println("Item1: " + item1 + " Price: " + price1 + " Item2: " + item2 + " Price: "+
-        price2 + " Price: " + " Item3: " + item3 + " Price: "+ price3);
-        
+		Scanner scan = new Scanner (System.in);
+	    
+	    int loginAttemps=3;
+	    
+	    for (int i=0; i<=loginAttemps;i++) {
+	    
+	        System.out.println("Enter username:");
+	        String userName=scan.next();
+	        System.out.println("Enter password:");
+	        String password=scan.next();
+	        
+	        if (i==2) {
+	            System.out.println("3 attemps failed!");
+	            System.out.println("Account locked!");
+	            break;
+	        }
+	        
+	        if (userName.equals("Cybertek") && (password.equals("Cybertek123"))) {
+	                System.out.println("Login sucessful.");    
+	                break;
+	                
+	        }
+	            if (! userName.equals("Cybertek") && ( ! password.equals("Cybertek123"))) {
+	                
+	                System.out.println("Username or password invalid!");
+	                System.out.println("Please re-enter! \n");
+	                
+	            
+	            }else {
+	                
+	        
+	    }
 		
-        
-        sc.close();
-	}
-	
+	    }
 
+	
+    }
+    
 }
+
