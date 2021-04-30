e6f140cdc5407bd957b51fd3fb9636f6cd87bb5c
hiltas73 <h_iltas@yahoo.com>
2021-04-30 14:40:40 +0300

    after A-10 Q-24

--- src/Assm10_/Assm10_q24_StringJavaPython.java
+++ src/Assm10_/Assm10_q24_StringJavaPython.java
@@ -8,8 +8,14 @@ public class Assm10_q24_StringJavaPython {
 //		Given a string, print out true if the number of appearances of "java" anywhere in the string is 
 //		equal to the number of appearances of "python" anywhere in the string (case sensitive).
 
-		String sample = "We study java not python";
+		String sample = "We study java not python. Java! Java";
 
+		System.out.println(numberOfString(sample));
+
+	}
+
+	private static boolean numberOfString(String sentence) {
+		
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Enter a first input=");
@@ -24,34 +30,33 @@ public class Assm10_q24_StringJavaPython {
 		int count1 = 0;
 		int count2 = 0;
 
-		for (int i = 0; i <= sample.length() - input1.length(); i++) {
+		for (int i = 0; i <= sentence.length() - input1.length(); i++) {
 
-			if (sample.substring(i, i + input1.length()).equals(input1)) {
+			if (sentence.substring(i, i + input1.length()).equals(input1)) {
 
 				count1++;
-				System.out.println("count1 " + count1);
-
 			}
-
 		}
 
-		for (int i = 0; i <= sample.length() - input2.length(); i++) {
+		for (int i = 0; i <= sentence.length() - input2.length(); i++) {
 
-			if (sample.substring(i, i + input2.length()).equals(input2)) {
+			if (sentence.substring(i, i + input2.length()).equals(input2)) {
 
 				count2++;
-				System.out.println("count2 " + count2);
 			}
-
 		}
+		
+		System.out.println("count1 " + count1);
+		System.out.println("count2 " + count2);
 
 		if (count1 == count2) {
 
-			System.out.println(true);
+			return true;
 
 		} else {
-			System.out.println(false);
+			return false;
 		}
-
+		
 	}
+	
 }
